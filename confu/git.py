01bbae9192ed7af9f22e67a2098f3cf6e48203a2
Marat Dukhan <maratek@gmail.com>
2017-02-16 00:04:11 -0500

    Git: complain only once about each HTTPS host

--- confu/git.py
+++ confu/git.py
@@ -5,13 +5,19 @@ import pygit2
 logger = logging.getLogger("confu")
 
 
+failed_certificate_hosts = set()
+
+
 class RemoteCallbacks(pygit2.RemoteCallbacks):
     def __init__(self, credentials=None, certificate=None):
         super(RemoteCallbacks, self).__init__(credentials, certificate)
 
     def certificate_check(self, certificate, valid, host):
         if not valid:
-            logger.warning("could not validate certificate for {host}".format(host=host))
+            # Do not complain twice about the same host
+            if host not in failed_certificate_hosts:
+                logger.warning("could not validate certificate for {host}".format(host=host))
+                failed_certificate_hosts.add(host)
 
         return True
 
